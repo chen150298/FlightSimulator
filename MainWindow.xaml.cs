5fb5b1fe89123c7291b436d67225a24e1b9b8cc7
chen150298 <73185261+chen150298@users.noreply.github.com>
2021-04-14 20:06:28 +0300

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using ap.Model;
+using ap.View;
+using ap.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ap
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    /// 
+    public partial class MainWindow : Window
+    {
+        public IPlaneModel planeModel;
+        public IClient client;
+        public ConnectViewModel ConnectViewModel;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+
+
+}
